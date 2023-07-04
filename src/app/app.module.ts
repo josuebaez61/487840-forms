a310982503536941816193591331718e4bb37df5
Josue Baez <josue.baez@coderhouse.com>
2023-07-04 19:00:33 -0300

    CHANGE, folder name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MyFormsModule } from './forms/my-forms.module';
+import { MyFormsModule } from './my-forms/my-forms.module';
 
 @NgModule({
   declarations: [
