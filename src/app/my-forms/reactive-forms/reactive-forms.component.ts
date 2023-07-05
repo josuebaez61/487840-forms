93482c0f8b291f340f5e240e13e5b4eff43f80bd
Josue Baez <josue.baez@coderhouse.com>
2023-07-04 21:07:14 -0300

    ADD, reactive forms

--- src/app/my-forms/reactive-forms/reactive-forms.component.ts
+++ src/app/my-forms/reactive-forms/reactive-forms.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+interface AddressFormGroup {
+  address: FormControl<string | null>;
+}
 
 interface UserModel {
   email: FormControl<string | null>;
   password: FormControl<string | null>;
   city: FormControl<string | null>;
+  addresses: FormArray<FormGroup<AddressFormGroup>>
 }
 
 @Component({
@@ -13,14 +18,65 @@ interface UserModel {
   styleUrls: ['./reactive-forms.component.scss']
 })
 export class ReactiveFormsComponent {
-  userModel: FormGroup<UserModel>;
+
+  /**
+   * FormControl
+   * Manejar el valor de una propiedad en especifico.
+   * Se vinculan directamente con los inputs, selects, textareas del html
+   */
+  emailControl = new FormControl('', [Validators.required]);
+  passwordControl = new FormControl('');
+  cityControl = new FormControl('', [Validators.required]);
+
+
+  /**
+   * FormArray
+   * Agrupan FormControl, FormArray y FromGroup
+   * A modo de array []
+   */
+
+
+  // FormArray<FormGroup<AddressFormGroup>>
+  // Aqui lo que decimos que dentro del FormArray
+  // Va a haber FormGroups, que dentro van a tener una prop "address"
+  // Que va a ser de tipo FromControl<string | null>
+  addresesFormArray = new FormArray<FormGroup<AddressFormGroup>>([
+    new FormGroup({
+      address: new FormControl(''),
+    }),
+  ]);
+
+
+  /**
+   * Los FormGroup agrupar FormControl, FormArray y FromGroup
+   * A modo de objeto {}
+   *
+   * {
+   *    email: '',
+   *    password: '',
+   *    city: '',
+   * }
+   */
+  userModel: FormGroup<UserModel> = new FormGroup({
+    email: this.emailControl,
+    password: this.passwordControl,
+    city: this.cityControl,
+    addresses: this.addresesFormArray,
+  });
+
+  addAddressControl(): void {
+    this.addresesFormArray.push(
+      this.fb.group({
+        address: [''],
+      })
+    )
+  }
 
   constructor(private fb: FormBuilder) {
-    this.userModel = this.fb.group({
-      email: [''],
-      password: [''],
-      city: [''],
-    });
-    console.log(this.userModel);
+    console.log(this.addresesFormArray);
+  }
+
+  deleteFormGroupFromAddressesFormArray(index: number): void {
+    this.addresesFormArray.removeAt(index);
   }
 }
